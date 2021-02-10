102d0cbf7accd481c4a39f3873164cf9922ca76b
Anant <anant.mathur007@gmail.com>
2021-02-10 13:53:14 +0530

    initial commit

---
+++ src/emails/account.js
@@ -0,0 +1,12 @@
+const sendWelcomeEmail = (email, name) => {
+  return;
+};
+
+const sendCancelationEmail = (email, name) => {
+  return;
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail,
+};
